f9d75f3f2da35b52650b05cada15e79043cf972c
xianchen1017 <1694809190@qq.com>
2025-04-09 17:22:55 +0800

    first commit

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import router from './router'
+
+const app = createApp(App)
+const pinia = createPinia()
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+app.use(ElementPlus)
+app.use(router)
+app.use(pinia)
+app.mount('#app')
